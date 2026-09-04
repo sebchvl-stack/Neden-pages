3338b9dbc119d914559da9d72f7ac1a770d4f678
sebchvl-stack <sebchvl@gmail.com>
2026-09-04 04:52:49 +0200

    feat: routes Home Privacy CGU

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Privacy from './pages/Privacy'
+import Terms from './pages/Terms'
+import Layout from './components/Layout'
+
+export default function App() {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/cgu" element={<Terms />} />
+      </Routes>
+    </Layout>
+  )
+}
